ae49acdf2f931593dc8934e78f07b6da9c180e63
Amjad <amjadcp03@gmail.com>
2021-07-10 21:51:36 +0530

    animals complited

---
+++ animals/animals.js
@@ -0,0 +1,20 @@
+var sum = 0
+
+/* function add(){
+       sum = sum + Number(document.frm1.one.value);
+       sum = sum + Number(document.frm1.two.value);
+       sum = sum + Number(document.frm1.three.value);
+       sum = sum + Number(document.frm2.four.value);
+       sum = sum + Number(document.frm2.five.value);
+       sum = sum + Number(document.frm2.six.value);
+       sum = sum + Number(document.frm3.seven.value);
+       sum = sum + Number(document.frm3.eight.value);
+       sum = sum + Number(document.frm3.nine.value);
+       sum = sum + Number(document.frm3.ten.value);
+       document.write(sum);
+} */
+
+function add(){
+       one = document.getElementsByValue('10');
+       document.write(one);
+}
